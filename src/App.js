3ab37083099290df1219ac1a3c13c42d34408c38
Lucas Miiller <lcorrea@unicadtec.com.br>
2020-08-23 20:15:02 -0300

    conceitos basicos react.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    api.post("repositories", {
+      title: "Aplicativo React.js",
+      url: "https://github.com/rocketseat-education/gostack-template-conceitos-reactjs",
+      techs: ["Node.js"]
+    }).then(response => {
+      setRepositories([...repositories, response.data]);
+    });
   }
 
+
+
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`repositories/${id}`).then(() => {
+
+      const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+      repositories.splice(repositoryIndex, 1);
+      setRepositories([...repositories]);
+
+
+      console.log("repositorio delatado com sucesso !")
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
           </button>
-        </li>
+          </li>
+        ))}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
